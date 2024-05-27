c7e33ba521ee1d2ec54be43506bc7c72d08061c8
apps2719 <83412353+apps2719@users.noreply.github.com>
2024-05-27 06:48:09 +0000

    project done

--- Blood_bank/Blood_bank/asgi.py
+++ Blood_bank/Blood_bank/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for Blood_bank project.
+ASGI config for Blood_Bank project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Blood_bank.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blood_Bank.settings')
 
 application = get_asgi_application()
--- Blood_bank/Blood_bank/wsgi.py
+++ Blood_bank/Blood_bank/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Blood_bank project.
+WSGI config for Blood_Bank project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Blood_bank.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Blood_Bank.settings')
 
 application = get_wsgi_application()
